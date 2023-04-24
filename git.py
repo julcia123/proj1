9229f772483ab05720baca657b88de81cf17de08
ania <aniachojnowska3@gmail.com>
2023-04-24 16:11:45 +0200

    dodalam neu i juz chyba calosc mamy

--- git.py
+++ git.py
@@ -44,6 +44,7 @@ class Transformations:
             return(sigma)
         
         
+        
         # XYZ ---> BLH - ALGORYTM HIRVONENA
             """
             Następujący algorytm przelicza współrzędne z układu ortokartezjańskiego na współrzędne geodezyjne.
@@ -63,16 +64,10 @@ class Transformations:
             return(fi, lam, h)
 
 
-        # BLH ---> XYZ
-
-        def filh2XYZ(self, fi, lam, h, output: 'dec_degree'):
-
-            lam = np.arctan2(Y, X)
-            return(fi, lam, h)
 
         # BLH ---> XYZ
             """
-           Algorytm przelicza współrzędne geodezyjne (BLH) na współrzędne w układzie ortokartezjańskim (XYZ)
+            Algorytm przelicza współrzędne geodezyjne (BLH) na współrzędne w układzie ortokartezjańskim (XYZ)
             """
         def filh2XYZ(self, fi, lam, h, output: 'dec_degree'):
             while True:
@@ -85,14 +80,115 @@ class Transformations:
                     break
             return(X, Y, Z)
 
+
+
         # XYZ ---> NEU
-        
-        
-        
-        
-        
-        
-        # Transformacja wsp BL -> 1992
+            """
+            Przeliczenie wspolrzednych puktow A i B z ukladu kartezjanskiego (XYZ) na geodezyjny (fi lam h)
+            """
+        def hirvonen(X, Y, Z, a, e2):
+            p = np.sqrt(X**2 + Y**2)
+            fi = np.arctan(Z / (p * (1 - e2)))
+            while True:
+                N = Np(fi, a, e2)
+                h = p / np.cos(fi) - N
+                fip = fi     #fip - fi poprzednie, fi - fi nowe
+                fi = np.arctan(Z / (p * (1 - N * e2 / (N + h))))
+                if abs(fip - fi) < (0.000001/206265):
+                    break
+            l = np.arctan2(Y, X)
+            return(fi, l, h)
+
+            """
+            Wyznaczenie azymutu i odleglosci miedzy punktami
+            """
+        def vincenty(self, fiA, lamA, fiB, lamB):
+            b = self.a * np.sqrt(1 - self.e2)
+            f = 1 - b / self.a
+            UA = np.arctan((1 - f) * np.tan(fiA))
+            UB = np.arctan((1 - f) * np.tan(fiB))
+            dl = lamB - lamA
+            
+            L = dl
+            while True:
+                sinsigma = np.sqrt(((np.cos(UB) * np.sin(L))**2) + ((np.cos(UA) * np.sin(UB) - np.sin(UA) * np.cos(UB) * np.cos(L))**2))
+                cossigma = np.sin(UA) * np.sin(UB) + np.cos(UA) * np.cos(UB) * np.cos(L)
+                sigma = np.arctan(sinsigma / cossigma)
+                sinAz = (np.cos(UA) * np.cos(UB) * np.sin(L)) / sinsigma
+                cos2Az = 1 - (sinAz)**2
+                cos2sigma = cossigma - ((2 * np.sin(UA) * np.sin(UB)) / cos2Az)
+                C = (f / 16) * cos2Az * (4 + f * (4 - 3 * cos2Az))
+                Lp = L
+                L = dl + (1 - C) * f * sinAz * (sigma + C * sinsigma * (cos2sigma + C * cossigma * (-1 + 2 * (cos2sigma)**2)))
+                if abs(L - Lp) < (0.000001/206265):
+                    break
+                
+                u2 = ((self.a**2 - b**2) / b**2) * cos2Az
+                A = 1 + (u2/16384) * (4096 + u2 * (-768 + u2 * (320 - 175 * u2)))
+                B = (u2/1024) * (256 + u2 * (-128 + u2 * (74 - 47 * u2)))
+                dsigma = B * sinsigma * (cos2sigma + 1/4 * B * (cossigma * (-1 + 2 * (cos2sigma)**2) - 1/6 * B * cos2sigma * (-3 + 4 * (sinsigma)**2) * (-3 + 4 * (cos2sigma)**2)))
+                sAB = b * A * (sigma - dsigma)
+                AzAB = np.arctan2((np.cos(UB) * np.sin(L)), ((np.cos(UA) * np.sin(UB)) - (np.sin(UA) * np.cos(UB) * np.cos(L))))
+                AzBA = np.arctan2((np.cos(UA) * np.sin(L)), ((-np.sin(UA) * np.cos(UB)) + (np.cos(UA) * np.sin(UB) * np.cos(L)))) + np.pi
+                if AzAB > 2 * np.pi:
+                    AzAB = AzAB - 2 * np.pi
+                if AzAB < 0:  
+                    AzAB = AzAB + 2 * np.pi
+                if AzBA > 2 * np.pi:
+                    AzBA = AzBA - 2 * np.pi
+                if AzBA < 0:
+                    AzBA = AzBA + 2 * np.pi
+                    
+            return(sAB, AzAB, AzBA)
+
+            """
+            Wyznaczenie kata zenitalnego
+            """
+        def zenitalny(self, fiA, lamA, hA, fiB, lamB, hB):
+            N_a = self.a/np.sqrt(1-self.e2*np.sin(fiA)**2)
+            R_a = np.array([(N_a+hA)*np.cos(fiA)*np.cos(lamA),
+                              (N_a+hA)*np.cos(fiA)*np.sin(lamA), 
+                              (N_a*(1-self.e2)+hA)*np.sin(fiA)])
+            
+            N_b = self.a/np.sqrt(1-self.e2*np.sin(fiB)**2)
+            R_b = np.array([(N_b+hB)*np.cos(fiB)*np.cos(lamB),
+                              (N_b+hB)*np.cos(fiB)*np.sin(lamB),
+                              (N_b*(1-self.e2)+hB)*np.sin(fiB)])
+            
+            R_delta = R_b - R_a
+            R_dlugosc  = np.linalg.norm(R_delta, 2)
+            
+            R = R_delta / R_dlugosc
+            
+            n  = np.array([-1 * np.sin(fiA)*np.cos(lamA),
+                          -1 * np.sin(fiA)*np.sin(lamA),
+                          np.cos(fiA)])
+            
+            e  = np.array([-1 * np.sin(lamA),
+                          np.cos(lamA),
+                          0]) 
+            
+            u  = np.array([np.cos(fiA)*np.cos(lamA),
+                  np.cos(fiA)*np.sin(lamA),
+                  np.sin(fiA)])
+            
+            cos_z = float(np.dot(u, R))
+            z = np.arccos(cos_z)
+            
+            return(z)
+
+            """
+            Wyznaczenie macierzy dneu
+            """
+        def saz2neu(sAB, AzAB, z):
+            dneu = np.array([sAB * np.sin(z) * np.cos(AzAB),
+                             sAB * np.sin(z) * np.sin(AzAB),
+                             sAB * np.cos(z)])
+            return(dneu)
+
+
+
+        # TRANSFORMACJA WSP BL ---> 1992
             """
             Algorytm przelicza współrzędne geodezyjne (BL) na współrzędne w układzie 1992 (XY)
             """
@@ -126,7 +222,8 @@ class Transformations:
                 return(x92, y92)
             
             
-        #Transformacja współrzędnych BL -> 2000
+            
+        # TRANSFORMACJA WSP BL ---> 2000
             """
             Następujący algorytm umożliwia przeliczenie współrzędnych geodezyjnych (BLH) na współrzędne w układzie 2000 (XY)
             """
