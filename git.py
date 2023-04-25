521748b023023a405046a97fbb2bc095997c5b8e
ania <aniachojnowska3@gmail.com>
2023-04-25 18:52:45 +0200

    zmienilam nam neu i poprawilam troche wyglad calego kodu

--- git.py
+++ git.py
@@ -9,24 +9,6 @@ import numpy as np
 import argparse
 
 
-#plik = 'belble.txt'
-#p = open('belble.txt', "r")
-#dane = p.readlines()
-
-#for i in dane:
-#    if "X" in i:
-#        X = float(i[3:15])
-#print('X = ', X)
-
-#for i in dane:
-#    if "Y" in i:
-#        Y = float(i[20:32])
-#print('Y = ', Y)
-
-#for i in dane:
-#    if "Z" in i:
-#        Z = float(i[38:49])
-#print('Z = ', Z)
 
 
 class Transformations:
@@ -37,10 +19,10 @@ class Transformations:
         """
         Parametry elipsoid:
         a - duża półos elipsoidy 
-        e - mimosród elipsoidy
-        + WGS84:
+        e2 - kwadrat mimosrodu elipsoidy
+            + WGS84:
             + GRS80:
-                + Elipsoida Krasowskiego:
+            + Elipsoida Krasowskiego:
         """
         if model == "WGS84":
             self.a = 6378137.000
@@ -53,7 +35,10 @@ class Transformations:
             self.e2 = 0.00669342162296
         else:
             raise AttributeError("Jesli napisałes małymi literami, spróbuj napisać wielkimi")
-    #Pomocnicze funkcje
+            #Pomocnicze funkcje
+            
+            
+            
         """
         Poniższe funkcje są funkcjami pomocniczymi dla obliczeń transformacji
         """
@@ -70,37 +55,13 @@ class Transformations:
            
         return(sigma)
     
-    def dms2deg(self, dms):
-        '''
-        This method convert deg, min, sek to decimal degree
-        '''
-        d = dms[0]; m = dms[1]; s = dms[2]
-        decimal_degree = d+m/60+s/3600
-        return (decimal_degree)
-    
-    def deg2dms(self, decimal_degree):
-        '''
-        This method convert decimal degree to deg, min, sek   
-        '''
-        decimal_degree = decimal_degree / 3600
-        dms = np.array([])
-        st = np.floor(decimal_degree)
-        append(dms, st)
-        m = np.floor((decimal_degree - st)*60)
-        append(dms, m)
-        sek = (decimal_degree - st - m/60)*3600
-        append(dms, sek)
-        print(dms)
-        return (dms)
-    
-        
-        
+
         
         # XYZ ---> BLH - ALGORYTM HIRVONENA
         """
             Następujący algorytm przelicza współrzędne z układu ortokartezjańskiego na współrzędne geodezyjne.
         """
-    def hirvonen(self, X, Y, Z, output: 'dms'):
+    def hirvonen(self, X, Y, Z):
         p = np.sqrt(X**2 + Y**2)
         fi = np.arctan(Z / (p * (1 - self.e2)))
         while True:
@@ -134,93 +95,41 @@ class Transformations:
 
 
         # XYZ ---> NEU
-
         """
-            Wyznaczenie azymutu i odleglosci miedzy punktami
+            Obliczenie macierzy Rneu
         """
-    def vincenty(self, fiA, lamA, fiB, lamB):
-        b = self.a * np.sqrt(1 - self.e2)
-        f = 1 - b / self.a
-        UA = np.arctan((1 - f) * np.tan(fiA))
-        UB = np.arctan((1 - f) * np.tan(fiB))
-        dl = lamB - lamA
-        
-        L = dl
+    def Rneu(self, fi, lam):
+        Rneu = np.array([[-np.sin(fi)*np.cos(lam), -np.sin(lam), np.cos(fi)*np.cos(lam)],
+                         [-np.sin(fi)*np.sin(lam),  np.cos(lam), np.cos(fi)*np.sin(lam)],
+                         [             np.cos(fi),            0,             np.sin(fi)]])
+        return Rneu
+    
+    
+        """
+            Przeliczenie wsp XYZ na neu
+        """
+    def xyz2neup(self, X, Y, Z, X0, Y0, Z0):
+        neu = []
+        p = np.sqrt(X0**2 + Y0**2)
+        fi = np.arctan(Z0 / (p*(1 - self.e2)))
         while True:
-            sinsigma = np.sqrt(((np.cos(UB) * np.sin(L))**2) + ((np.cos(UA) * np.sin(UB) - np.sin(UA) * np.cos(UB) * np.cos(L))**2))
-            cossigma = np.sin(UA) * np.sin(UB) + np.cos(UA) * np.cos(UB) * np.cos(L)
-            sigma = np.arctan(sinsigma / cossigma)
-            sinAz = (np.cos(UA) * np.cos(UB) * np.sin(L)) / sinsigma
-            cos2Az = 1 - (sinAz)**2
-            cos2sigma = cossigma - ((2 * np.sin(UA) * np.sin(UB)) / cos2Az)
-            C = (f / 16) * cos2Az * (4 + f * (4 - 3 * cos2Az))
-            Lp = L
-            L = dl + (1 - C) * f * sinAz * (sigma + C * sinsigma * (cos2sigma + C * cossigma * (-1 + 2 * (cos2sigma)**2)))
-            if abs(L - Lp) < (0.000001/206265):
-                break
-                
-            u2 = ((self.a**2 - b**2) / b**2) * cos2Az
-            A = 1 + (u2/16384) * (4096 + u2 * (-768 + u2 * (320 - 175 * u2)))
-            B = (u2/1024) * (256 + u2 * (-128 + u2 * (74 - 47 * u2)))
-            dsigma = B * sinsigma * (cos2sigma + 1/4 * B * (cossigma * (-1 + 2 * (cos2sigma)**2) - 1/6 * B * cos2sigma * (-3 + 4 * (sinsigma)**2) * (-3 + 4 * (cos2sigma)**2)))
-            sAB = b * A * (sigma - dsigma)
-            AzAB = np.arctan2((np.cos(UB) * np.sin(L)), ((np.cos(UA) * np.sin(UB)) - (np.sin(UA) * np.cos(UB) * np.cos(L))))
-            AzBA = np.arctan2((np.cos(UA) * np.sin(L)), ((-np.sin(UA) * np.cos(UB)) + (np.cos(UA) * np.sin(UB) * np.cos(L)))) + np.pi
-            if AzAB > 2 * np.pi:
-                AzAB = AzAB - 2 * np.pi
-            if AzAB < 0:  
-                AzAB = AzAB + 2 * np.pi
-            if AzBA > 2 * np.pi:
-                AzBA = AzBA - 2 * np.pi
-            if AzBA < 0:
-                AzBA = AzBA + 2 * np.pi
-                    
-        return(sAB, AzAB, AzBA)
-
-        """
-            Wyznaczenie kata zenitalnego
-        """
-    def zenitalny(self, fiA, lamA, hA, fiB, lamB, hB):
-        N_a = self.a/np.sqrt(1-self.e2*np.sin(fiA)**2)
-        R_a = np.array([(N_a+hA)*np.cos(fiA)*np.cos(lamA),
-                (N_a+hA)*np.cos(fiA)*np.sin(lamA), 
-                (N_a*(1-self.e2)+hA)*np.sin(fiA)])
-            
-        N_b = self.a/np.sqrt(1-self.e2*np.sin(fiB)**2)
-        R_b = np.array([(N_b+hB)*np.cos(fiB)*np.cos(lamB),
-                       (N_b+hB)*np.cos(fiB)*np.sin(lamB),
-                       (N_b*(1-self.e2)+hB)*np.sin(fiB)])
-            
-        R_delta = R_b - R_a
-        R_dlugosc  = np.linalg.norm(R_delta, 2)
-            
-        R = R_delta / R_dlugosc
+            N = self.Npu(fi)
+            h = (p / np.cos(fi)) - N
+            fi_poprzednia = fi
+            fi = np.arctan((Z0 / p)/(1-((N * self.e2)/(N + h))))
+            if abs(fi_poprzednia - fi) < (0.000001/206265):
+                break 
+        N = self.Npu(fi)
+        h = p/np.cos(fi) - N
+        lam = np.arctan(Y0 / X0)
         
-        n  = np.array([-1 * np.sin(fiA)*np.cos(lamA),
-                       -1 * np.sin(fiA)*np.sin(lamA),
-                       np.cos(fiA)])
+        R_neu = self.Rneu(fi, lam)
+        for X, Y, Z in zip(X, Y, Z):
+            X_sr = [X - X0, Y - Y0, Z - Z0] 
+            X_rneu = R_neu.T@X_sr
+            neu.append(X_rneu.T)
             
-        e  = np.array([-1 * np.sin(lamA),
-                         np.cos(lamA),
-                         0]) 
-            
-        u  = np.array([np.cos(fiA)*np.cos(lamA),
-                  np.cos(fiA)*np.sin(lamA),
-                  np.sin(fiA)])
-            
-        cos_z = float(np.dot(u, R))
-        z = np.arccos(cos_z)
-            
-        return(z)
-
-    """
-            Wyznaczenie macierzy dneu
-        """
-    def saz2neu(sAB, AzAB, z):
-        dneu = np.array([sAB * np.sin(z) * np.cos(AzAB),
-                             sAB * np.sin(z) * np.sin(AzAB),
-                             sAB * np.cos(z)])
-        return(dneu)
+        return neu
 
 
 
@@ -228,7 +137,7 @@ class Transformations:
         """
             Algorytm przelicza współrzędne geodezyjne (BL) na współrzędne w układzie 1992 (XY)
         """
-    def cale92(self,fi, lam):
+    def cale92(self, fi, lam):
         lam0 = (19*np.pi)/180
                 
         m = 0.9993
